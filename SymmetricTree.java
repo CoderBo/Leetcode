424cafd1952e2679f1b8a499b6c29c084bfed4b4
CoderBo <1013278176@qq.com>
2014-02-18 16:12:42 -0500

    增加一种递归算法

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -54,5 +54,24 @@ public class SymmetricTree
         }
          return true;
     }
+    
+    static boolean isSymmetric2(TreeNode root) {
+        if(root == null) return true;
+        else return isSymmetric(root.left, root.right);
+    }
+    static boolean isSymmetric(TreeNode root1, TreeNode root2)
     {
+        if(root1 == null && root2 == null) return true;
+        if(root1 != null && root2 == null) return false;
+        if(root2 != null && root1 == null) return false;
+        if(root1.val != root2.val) return false;
+        else return isSymmetric(root1.left, root2.right) && isSymmetric(root1.right, root2.left);
+    }
+    public static void main(String[] args)
+    {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        System.out.println(isSymmetric2(root));
+    }
 }
