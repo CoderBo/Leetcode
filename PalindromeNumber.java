b4b5c577a9da1138d32e35b76f68e1e10e5100c4
CoderBo <1013278176@qq.com>
2014-03-07 13:27:46 -0500

    算法一样，重新整理

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -6,60 +6,35 @@
 package leetcode;
 
 /**
- * problem:
+ * problem:Determine whether an integer is a palindrome. Do this without extra space.
  * @author chenbo
  */
 public class PalindromeNumber 
 {
-    public static int inverse(int n)
-    {
-        int sub = 0;
-        int result = 0;
-        while(n > 0)
-        {
-            sub = n % 10;
-            n = n / 10;
-            result = (result + sub) * 10;
-        }
-        return result / 10;
-    }   
-    static int getLength(int n)
-    {
-        int count = 0;
-        while(n != 0)
-        {
-            count++;
-            n /= 10;
-        }
-        return count;
-    }
-    static boolean isPalindrome(int x)
-    {
+    public static boolean isPalindrome(int x) {
         if(x < 0) return false;
-        int front = x / (int)(Math.pow(10, getLength(x) / 2));
-        int end = x - front * (int)(Math.pow(10, getLength(x) / 2));
-        front = getLength(x) % 2 == 0 ? front:front / 10;
-        return inverse(front) == end;
+        int div = 1, head = 0, tail = 0;
+        while(x / div >= 10)
+        {
+            div *= 10;
+        }
+        while(x >= 10)
+        {
+            head = x / div;
+            tail = x % 10;
+            if(head != tail) return false;
+            else
+            {
+                x = x - head * div;
+                x /= 10;
+                div /= 100;
+            }
+        }
+        return true;
     }
-  static boolean isPalindrome2(int x) {
-  if (x < 0) return false;
-  int div = 1;
-  while (x / div >= 10) {
-    div *= 10;
-  }        
-  while (x != 0) {
-      System.out.println(x + " " + div);
-    int l = x / div;
-    int r = x % 10;
-    if (l != r) return false;
-    x = (x % div) / 10;
-    div /= 100;
-  }
-  return true;
-}
     public static void main(String[] args)
     {
-        System.out.println(isPalindrome2(1000110001));
+        System.out.println(isPalindrome(1000110001));
        // System.out.println(inverse(86));
     }
    
