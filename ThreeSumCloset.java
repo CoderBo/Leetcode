67290e8dac16bc6b2ade86407e71ddcb11b2a422
CoderBo <1013278176@qq.com>
2014-03-14 23:58:50 -0400

    简化了一下
    已经记不清第一次写的思路了……这次是用长度为2的数组存位置i和j.

--- ThreeSumCloset.java
+++ ThreeSumCloset.java
@@ -14,54 +14,51 @@ import static leetcode.ThreeSum.threeSum;
  * @author chenbo
  */
 public class ThreeSumCloset 
-{
-    static int twoSumClosest(int[] num, int target, int start)
-    {    
-        int min = Integer.MAX_VALUE;
-        int result = 0;
-        int i = start;
-        int j = num.length - 1;
-        while(i < j)
-        {
-            int sum = num[i] + num[j];
-            int temp = Math.abs(target - sum);
-            if(temp < min) 
-            {
-                min = temp;
-                result = sum;
-            }
-            if(sum == target) return target;
-            else if(sum < target) i++;
-            else j--;
-        }
-        return result;
-    }
-    static int threeSumClosest(int[] num, int target)
-    {
+{    
+    static int threeSumClosest(int[] num, int target) {
         Arrays.sort(num);
+        if(num.length <= 2) return 0;
+        int minDifference = Integer.MAX_VALUE;
         int result = 0;
-        int min = Integer.MAX_VALUE;
+        int[] position = new int[2];
         for(int i = 0; i < num.length - 2; i++)
         {
-            int target2 = target - num[i];
-            //System.out.println("targe2:" + target2);
-            int sub = twoSumClosest(num, target2, i + 1);
-            //System.out.println("sub:" + sub);
-            int value = Math.abs(target - sub - num[i]);
-            if(value < min)
+            int difference = twoSumCloset(num, target - num[i], i + 1, num.length - 1, position);
+            if(difference <  minDifference) 
             {
-                min = value;
-                result = sub + num[i];
+                minDifference = difference;
+                result = num[i] + num[position[0]] + num[position[1]];
             }
         }
         return result;
     }
+    static int twoSumCloset(int[] num, int target, int start, int end, int[] position)
+    {
+        int i = start;
+        int j = end;
+        int minDifference = Integer.MAX_VALUE;
+        int result = 0;
+        while(i < j)
+        {
+            int difference = Math.abs(target - (num[i] + num[j]));
+            if(difference < minDifference) 
+            {
+                minDifference = difference;
+                position[0] = i;
+                position[1] = j;
+            }
+            if(num[i] + num[j] < target) i++;
+            else j--;
+        }
+        return minDifference;
+    }
+    
     public static void main(String[] arg)
     {
         int[] num = {8,-15,-2,-13,8,5,6,-3,-9,3,6,-6,8,14,-9,-8,-9,-6,-14,5,-7,3,-10,-14,-12,-11,12,-15,-1,12,8,-8,12,13,-13,-3,-5,0,10,2,-11,-7,3,4,-8,9,3,-10,11,5,10,11,-7,7,12,-12,3,1,11,9,-9,-4,9,-12,-6,11,-7,4,-4,-12,13,-8,-12,2,3,-13,-12,-8,14,14,12,9,10,12,-6,-1,8,4,8,4,-1,14,-15,-7,9,-14,11,9,5,14};
-        int[] num2 = {1,2,0};
+        int[] num2 = {0,0,0};
         int[] num3 = {1,2};
-        System.out.println(threeSumClosest(num2, 0));
-        System.out.println(twoSumClosest(num3, 0, 0));
+        System.out.println(threeSumClosest(num, 1));
+        //System.out.println(twoSumClosest(num3, 0, 0));
     }
 }
