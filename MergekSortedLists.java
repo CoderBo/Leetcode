a0f2094280dd683928e2c3a379480fd2a5522140
CoderBo <1013278176@qq.com>
2014-03-23 23:46:22 -0400

    添加一种用递归merge两个sorted list的方法，真心简单明了

--- MergekSortedLists.java
+++ MergekSortedLists.java
@@ -63,7 +63,31 @@ public class MergekSortedLists
         }      
         return head;
     }
-    
+  /////////////////////////////////////////////////////////////////  
+    ListNode mergeTwo(ListNode l1, ListNode l2)
+    {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.val < l2.val)
+        {
+            l1.next = mergeTwo(l1.next, l2);
+            return l1;
+        }
+        else
+        {
+            l2.next = mergeTwo(l1, l2.next);
+            return l2;
+        }
+    }
+    public ListNode mergeKLists2(ArrayList<ListNode> lists) {
+        ListNode head = null;
+        for(ListNode l : lists)
+        {
+            head = mergeTwo(head, l);
+        }
+        return head;
+    }
+/////////////////////////////////////////////////////////////////
     static ListNode mergeKLists(ArrayList<ListNode> lists)
     {
         ListNode result = new ListNode(Integer.MIN_VALUE);
