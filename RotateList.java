0cad9d5f0c0b4a45788c1279694b554728bbf500
CoderBo <1013278176@qq.com>
2014-03-27 23:48:25 -0400

    无

--- RotateList.java
+++ RotateList.java
@@ -45,11 +45,11 @@ public class RotateList
     {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
-      //  head.next.next = new ListNode(3);
-      //  head.next.next.next = new ListNode(4);
-      //  head.next.next.next.next = new ListNode(5);
-      //  head.next.next.next.next.next = null;
-        head = rotateRight(head, 2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = null;
+        head = rotateRight(head, 1);
         while(head != null)
         {
             System.out.println(head.val);
