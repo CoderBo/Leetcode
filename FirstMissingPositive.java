1775f393436d967e90ad717d86bcc33289c829c2
CoderBo <1013278176@qq.com>
2014-03-29 01:47:45 -0400

    无

--- FirstMissingPositive.java
+++ FirstMissingPositive.java
@@ -53,9 +53,29 @@ public class FirstMissingPositive
         return j;
     }
     
+    static int firstMissingPositive2(int[] A) {
+        for(int i = 0; i < A.length;)
+        {
+            if(i == A[i] - 1) i++;
+            else if(A[i] > 0 && A[i] <= A.length)
+            {
+                int index = A[i] - 1;
+                int temp = A[i];
+                A[i] = A[index];
+                A[index] = temp;
+                if(A[i] == A[index]) i++;
+            }
+            else i++;
+        }
+        for(int i = 0; i < A.length; i++)
+        {
+            if(i != A[i] - 1) return i + 1;
+        }
+        return A.length + 1;
+    }
     public static void main(String[] args)
     {
         int []array = {1,1,1,1,1};
-        System.out.println(firstMissingPositive(array));
+        System.out.println(firstMissingPositive2(array));
     }
 }
