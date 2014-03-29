43e1d9f5b03ab35a762a252b92847842834067ab
CoderBo <1013278176@qq.com>
2014-03-29 01:47:51 -0400

    无

--- ScrambleString.java
+++ ScrambleString.java
@@ -36,11 +36,26 @@ public class ScrambleString
         }
         return false;
     }
+    
+    static boolean isScramble2(String s1, String s2) {
+        if(s1.length() != s2.length()) return false;
+        if(s1.equals(s2)) return true;
+        if(s1.length() == 1 && (!s1.equals(s2))) return false;
+        for(int i = 1; i < s1.length(); i++)
+        {
+            String s1Left = s1.substring(0, i);
+            String s1Right = s1.substring(i);
+            String s2Left = s2.substring(0, s2.length() - i);
+            String s2Right = s2.substring(s2.length() - i);
+            if(isScramble(s1Left, s2Right) && isScramble(s1Right, s2Left)) return true;
+        }
+        return false;
+    }
     public static void main(String[] args)
     {
         System.out.println('a');
         String s = "great";
 
-        System.out.println(isScramble("abcd", "edab"));
+        System.out.println(isScramble2("xstjzkfpkggnhjzkpfjoguxvkbuopi", "xbouipkvxugojfpkzjhnggkpfkzjts"));
     }
 }
